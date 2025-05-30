5aa9d11e95d7ff5b102534f0cc46a6b6756cba7b
OrlandoMartinho <srsaiombo@gmail.com>
2025-05-30 22:00:21 +0100

    fixing any bugs

--- config/config.sql
+++ config/config.sql
@@ -1,94 +1,89 @@
--- Criar o banco de dados (ajuste o nome se desejar)
-CREATE DATABASE IF NOT EXISTS sistema_de_inscricao_v2;
-USE sistema_de_inscricao_v2;
+CREATE DATABASE IF NOT EXISTS `sistema_de_inscricao_v2`;
+USE `sistema_de_inscricao_v2`;
 
--- Tabela: admin_users
-CREATE TABLE IF NOT EXISTS admin_users (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(50) NOT NULL UNIQUE,
-    password VARCHAR(255) NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
+CREATE TABLE IF NOT EXISTS `admin_users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `username` (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+INSERT INTO `admin_users` (`id`, `username`, `password`, `created_at`) VALUES
+(2, 'admin', '$2y$10$rnaXLEKVQkGO8qS.FzL2I.HfimOZvpbHqOH8QWKJGzYuXqWYQL7Bi', '2025-05-27 04:57:22');
 
+CREATE TABLE IF NOT EXISTS `atividades` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `acao` varchar(255) NOT NULL,
+  `usuario` varchar(100) NOT NULL,
+  `data_acao` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- Tabela: notificacoes
-CREATE TABLE IF NOT EXISTS notificacoes (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    data_notificacao DATETIME NOT NULL,
-    descricao VARCHAR(255),
-    admin_id INT,
-    FOREIGN KEY (admin_id) REFERENCES admin_users(id)
-        ON DELETE SET NULL
-        ON UPDATE CASCADE
-);
+CREATE TABLE IF NOT EXISTS `contactos` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `assunto` varchar(200) NOT NULL,
+  `mensagem` text NOT NULL,
+  `data_envio` timestamp NOT NULL DEFAULT current_timestamp(),
+  `status` enum('não lido','lido','respondido') DEFAULT 'não lido',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- Tabela: cursos
-CREATE TABLE IF NOT EXISTS cursos (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    nome VARCHAR(100) NOT NULL,
-    descricao TEXT NOT NULL,
-    duracao VARCHAR(50) NOT NULL,
-    nivel VARCHAR(20) NOT NULL,
-    instrutor VARCHAR(100) NOT NULL,
-    status ENUM('ativo', 'inativo') DEFAULT 'ativo',
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-);
+CREATE TABLE IF NOT EXISTS `cursos` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(100) NOT NULL,
+  `descricao` text NOT NULL,
+  `duracao` varchar(50) NOT NULL,
+  `nivel` varchar(20) NOT NULL,
+  `instrutor` varchar(100) NOT NULL,
+  `status` enum('ativo','inativo') DEFAULT 'ativo',
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- Tabela: inscricoes
-CREATE TABLE IF NOT EXISTS inscricoes (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    nome_completo VARCHAR(100) NOT NULL,
-    email VARCHAR(100) NOT NULL,
-    telefone VARCHAR(20) NOT NULL,
-    bi_numero VARCHAR(50) NOT NULL,
-    sexo ENUM('Masculino', 'Feminino') NOT NULL,
-    curso_id INT NOT NULL,
-    foto_passe LONGBLOB ,
-    documento_bi LONGBLOB ,
-    comprovativo LONGBLOB ,
-    data_inscricao TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    data_nascimento DATE NOT NULL,
-    status ENUM('pendente', 'aprovado', 'rejeitado') DEFAULT 'pendente',
-    FOREIGN KEY (curso_id) REFERENCES cursos(id)
-        ON DELETE CASCADE
-        ON UPDATE CASCADE
-);
+CREATE TABLE IF NOT EXISTS `eventos` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `titulo` varchar(100) NOT NULL,
+  `descricao` text DEFAULT NULL,
+  `data_evento` datetime DEFAULT NULL,
+  `local` varchar(100) DEFAULT NULL,
+  `foto` longblob DEFAULT NULL,
+  `data_criacao` timestamp NOT NULL DEFAULT current_timestamp(),
+  `status` varchar(10) NOT NULL DEFAULT 'ativo',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- Tabela: eventos
-CREATE TABLE  IF NOT EXISTS eventos (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    titulo VARCHAR(255) NOT NULL,
-    data_evento DATETIME NOT NULL,
-    local VARCHAR(255) NOT NULL,
-    descricao TEXT,
-    foto LONGBLOB, 
-    imagem VARCHAR(255), -- caminho da imagem ou nome do arquivo salvo
-    status ENUM('ativo', 'inativo') NOT NULL DEFAULT 'ativo',
-    criado_em TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
+CREATE TABLE IF NOT EXISTS `inscricoes` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nome_completo` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `telefone` varchar(20) NOT NULL,
+  `bi_numero` varchar(50) NOT NULL,
+  `sexo` enum('Masculino','Feminino') NOT NULL,
+  `curso_id` int(11) DEFAULT NULL,
+  `foto_passe` longblob DEFAULT NULL,
+  `documento_bi` longblob DEFAULT NULL,
+  `comprovativo` longblob DEFAULT NULL,
+  `data_inscricao` timestamp NOT NULL DEFAULT current_timestamp(),
+  `status` enum('pendente','aprovado','rejeitado') DEFAULT 'pendente',
+  `observacoes` varchar(255) NOT NULL,
+  `data_de_nascimento` DATE DEFAULT NULL,
+  `curso` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `curso_id` (`curso_id`),
+  CONSTRAINT `inscricoes_ibfk_1` FOREIGN KEY (`curso_id`) REFERENCES `cursos` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
-
--- Tabela: contactos
-CREATE TABLE IF NOT EXISTS contactos (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    nome VARCHAR(100) NOT NULL,
-    email VARCHAR(100) NOT NULL,
-    assunto VARCHAR(200) NOT NULL,
-    mensagem TEXT NOT NULL,
-    data_envio TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    status ENUM('não lido', 'lido', 'respondido') DEFAULT 'não lido'
-);
-
-CREATE TABLE IF NOT EXISTS atividades (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    acao VARCHAR(255) NOT NULL,
-    usuario VARCHAR(100) NOT NULL,
-    data_acao DATETIME NOT NULL
-);
-
-
-ALTER TABLE `inscricoes` ADD `observacoes` VARCHAR(255) NOT NULL AFTER `status`;
-
-ALTER TABLE `inscricoes` ADD `curso` VARCHAR(255) NOT NULL AFTER `observacoes`;
+CREATE TABLE IF NOT EXISTS `notificacoes` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `data_notificacao` datetime NOT NULL,
+  `descricao` varchar(255) DEFAULT NULL,
+  `admin_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `admin_id` (`admin_id`),
+  CONSTRAINT `notificacoes_ibfk_1` FOREIGN KEY (`admin_id`) REFERENCES `admin_users` (`id`) ON DELETE SET NULL ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
